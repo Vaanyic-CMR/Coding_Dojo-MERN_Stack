60d7d778ea02cd5f3e46db197db1e4b1bc92c4be
Caleb M Rank <calebmrank@gmail.com>
2022-03-25 09:35:42 -0600

    commit 03/25/2022

---
+++ 06-oop_&_class_components/01-user/user.js
@@ -0,0 +1,30 @@
+class User {
+    constructor( username, email ) {
+        this.name = username;
+        this.email = email;
+        this.accountBalance = 0;
+    };
+    makeDeposit( amount ) { this.accountBalance += amount; return this; };
+    makeWithdrawl( amount ) { this.accountBalance -= amount; return this; };
+    displayBalance() { console.log( `User: ${this.name}, Balance: $${this.accountBalance}` ); };
+    transferMoney( user, amount ) { this.makeWithdrawl(amount); user.makeDeposit( amount ); };
+}
+
+const user1 = new User( "Guido", "g@email.com" )
+const user2 = new User( "Anna", "a@email.com" )
+const user3 = new User( "Caleb", "c@email.com" )
+
+user1.makeDeposit(50).makeDeposit(200).makeDeposit(300).makeWithdrawl(500);
+user1.displayBalance();
+
+user2.makeDeposit(50).makeDeposit(200).makeWithdrawl(20).makeWithdrawl(30);
+user2.displayBalance();
+
+user3.makeDeposit(500).makeWithdrawl(100).makeWithdrawl(150).makeWithdrawl(50);
+user3.displayBalance();
+
+console.log("-----|BONUS|-----")
+
+user2.transferMoney( user1, 50 );
+user1.displayBalance();
+user2.displayBalance();
