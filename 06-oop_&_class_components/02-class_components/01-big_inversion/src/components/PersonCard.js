26a98ffd75301195bbf644f56f6dd9d8e941a23c
Caleb M Rank <calebmrank@gmail.com>
2022-03-30 10:09:52 -0600

    commit 03/30/2022

---
+++ 06-oop_&_class_components/02-class_components/01-big_inversion/src/components/PersonCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PersonCard extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{ props.lastName }, { props.firstName }</h1>
+                <p>Age: { props.age }</p>
+                <p>Hair Color: { props.hairColor }</p>
+            </div>
+        );
+    };
+};
+export default PersonCard;
