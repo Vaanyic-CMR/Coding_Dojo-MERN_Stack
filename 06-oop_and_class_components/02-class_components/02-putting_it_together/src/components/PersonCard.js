1cb68e0df954d176e92bb71b590100a35284ddc8
Caleb M Rank <calebmrank@gmail.com>
2022-03-31 09:38:12 -0600

    commit 03/31/2022

---
+++ 06-oop_and_class_components/02-class_components/02-putting_it_together/src/components/PersonCard.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class PersonCard extends Component {
+    constructor( props ) {
+        super( props );
+        this.state = { age: props.age }
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>{ this.props.lastName }, { this.props.firstName }</h1>
+                <p>Age: { this.state.age }</p>
+                <p>Hair Color: { this.props.hairColor }</p>
+                <button onClick={ () => this.setState({ age: this.state.age+1 }) } >
+                    Birthday Button for { this.props.firstName } { this.props.lastName }
+                </button>
+                <hr style={{ margin: "30px 10%" }}/>
+            </div>
+        );
+    };
+};
+export default PersonCard;
